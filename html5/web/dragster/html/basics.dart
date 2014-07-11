8f0241379642909a05b359b7154534f66e07fb50
Robert Cooke <robert.cooke@trimm.nl>
2014-07-11 10:04:34 +0200

    renamed project to dragster

---
+++ html5/web/dragster/html/basics.dart
@@ -0,0 +1,81 @@
+
+import 'dart:html';
+
+class Basics {
+  Element _dragSourceEl;
+
+  void start() {
+    var cols = document.querySelectorAll('#columns .column');
+    for (var col in cols) {
+      col.onDragStart.listen(_onDragStart);
+      col.onDragEnd.listen(_onDragEnd);
+      col.onDragEnter.listen(_onDragEnter);
+      col.onDragOver.listen(_onDragOver);
+      col.onDragLeave.listen(_onDragLeave);
+      col.onDrop.listen(_onDrop);
+      col.onClick.listen(_onClickResize);
+    }
+  }
+
+
+  void _onClickResize(MouseEvent event) {
+    Element resizeTarget = event.target;
+    resizeTarget.style.setProperty('overflow', 'auto');
+  }
+
+  void _onDragStart(MouseEvent event) {
+    Element dragTarget = event.target;
+    dragTarget.classes.add('moving');
+    _dragSourceEl = dragTarget;
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('text/html', dragTarget.innerHtml);
+  }
+
+  void _onDragEnd(MouseEvent event) {
+    Element dragTarget = event.target;
+    dragTarget.classes.remove('moving');
+    var cols = document.querySelectorAll('#columns .column');
+    for (var col in cols) {
+      col.classes.remove('over');
+    }
+  }
+
+  void _onDragEnter(MouseEvent event) {
+    Element dropTarget = event.target;
+    dropTarget.classes.add('over');
+  }
+
+  void _onDragOver(MouseEvent event) {
+    // This is necessary to allow us to drop.
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }
+
+  void _onDragLeave(MouseEvent event) {
+    Element dropTarget = event.target;
+    dropTarget.classes.remove('over');
+  }
+
+  void _onDrop(MouseEvent event) {
+    // Stop the browser from redirecting.
+    event.stopPropagation();
+
+    // Don't do anything if dropping onto the same column we're dragging.
+    Element dropTarget = event.target;
+    if (_dragSourceEl != dropTarget) {
+      // Set the source column's HTML to the HTML of the column we dropped on.
+      _dragSourceEl.style.setProperty('overflow', 'visible');
+      _dragSourceEl.setInnerHtml(dropTarget.innerHtml, treeSanitizer: new NullTreeSanitizer());
+      dropTarget.setInnerHtml(event.dataTransfer.getData('text/html'), treeSanitizer: new NullTreeSanitizer());
+    }
+  }
+}
+
+void main() {
+  var basics = new Basics();
+  basics.start();
+}
+
+class NullTreeSanitizer implements NodeTreeSanitizer {
+  void sanitizeTree(Node node) {}
+}
